02a255899a48fbdd35e05577f2f67442ee3fb8b6
Jeshua <79337598+JJeshua@users.noreply.github.com>
2024-04-26 22:04:07 -0700

    Clear MongoDB Script.

--- db/clear_db.py
+++ db/clear_db.py
@@ -1,1 +1,24 @@
-print("Clearing DB.")
+import pymongo
+
+
+def clear_all_collections(db):
+    for collection_name in db.list_collection_names():
+        db[collection_name].delete_many({})
+        print(f"Cleared collection: {collection_name}")
+
+
+print("Clearing MongoDB.")
+
+mongo_client = pymongo.MongoClient("mongodb://localhost:27017")
+db_name = "DuckPond"
+
+# Connect to the database
+db = mongo_client[db_name]
+
+# Clear all collections
+clear_all_collections(db)
+
+# Close the MongoDB connection
+mongo_client.close()
+
+print("Successfully cleared MongoDB")
