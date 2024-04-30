bd209eceeac4145f1d8de03f0b2b8759abed395b
Jeshua <79337598+JJeshua@users.noreply.github.com>
2024-04-30 16:22:31 -0700

    I cannot get import to work.

--- db/populate_db.py
+++ db/populate_db.py
@@ -1,1 +1,32 @@
+import pymongo
+
+
+from tests.base_test import BaseTestClass
+
+
+NUM_OF_USERS = 100
+
+
+def clear_all_collections(db):
+    for collection_name in db.list_collection_names():
+        db[collection_name].delete_many({})
+        print(f"Cleared collection: {collection_name}")
+
+
 print('Populating DB.')
+
+mongo_client = pymongo.MongoClient("mongodb://localhost:27017")
+db_name = "DuckPond"
+
+# Connect to the database
+db = mongo_client[db_name]
+
+posts = []
+
+users = [BaseTestClass() for _ in range(NUM_OF_USERS)]
+
+
+# Close the MongoDB connection
+mongo_client.close()
+
+print("Successfully populated MongoDB")
